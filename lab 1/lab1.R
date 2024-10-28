23559ed31235fc00182a40ac4a2d0cc14d0ecce1
Rafin Abrar Rono <121242837+RafinRono@users.noreply.github.com>
2024-10-28 18:19:16 +0600

    Add files via upload

---
+++ lab 1/lab1.R
@@ -0,0 +1,64 @@
+1+1
+2+6
+26-89
+8*6
+65/5
+25**0.5
+5%%2
+sqrt(6465)
+pi * 5
+pi**6/7 + 1 - 16
+129.6 %% 23
+sqrt(5637**2 - 4521)
+10L
+10.6L
+x = 1
+x <- 1
+y <- 6
+z <- 8
+x * y * z
+x ** y / z * 2
+y** 10 %% 16
+X
+x
+X <- x
+X
+
+# testing testing 
+
+abs(-46)
+abs(6)
+ceiling(4.8)
+floor(4.8)
+
+tolower('HELLO')
+toupper('hellowu')
+strsplit('Let me see', split="")
+test <- strsplit('Let me see', split=" ")
+toupper(test)
+tolower(test)
+test
+strsplit('Let me see', split="e")
+
+nchar('okay')
+paste('okiay', 'buddy') #automatically adds a space in between
+paste('okiay', 'buddy', sep = "")
+paste0('okiay', 'buddy')
+
+str <- 'hello there,
+how is it going?,
+hope you are well'
+
+str
+cat(str)
+
+a <- 60
+b <- 90
+
+# if (cond) statement1 else statement2
+
+if(a>b){
+  print('a is greater than b')
+  }else
+{print('b is greater than a')}
+
